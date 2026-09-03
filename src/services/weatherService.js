b4437731a418bf10d0397f3998831aaf2e32d4ec
LockVision <hassanelmaayati@gmail.com>
2026-09-03 13:23:49 +0300

    .

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -1,21 +1,23 @@
-const API_KEY = "6bc9e708eafa4f4ea56131849240708";
+const API_KEY = '6bc9e708eafa4f4ea56131849240708';
 const BASE_URL = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}`;
 
+
+
 const show = async (city) => {
-  const queryStringURL = `${BASE_URL}&q=${city}`;
+  const queryStringURL = `${BASE_URL}&q=${city}`
 
   try {
-    const jsonRes = await fetch(queryStringURL);
-    const data = await jsonRes.json();
+    const jsonRes = await fetch(queryStringURL)
+    const data = await jsonRes.json()
 
     return data
+
   } catch (error) {
-    console.log(error.message);
+    console.log(error)
   }
-};
 
+}
 
-export {
-
-  show,
+export ()
+  show
 }
