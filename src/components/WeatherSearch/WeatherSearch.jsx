827e69b9ea1ffda5c0a7b144b142f6f3f7f27445
LockVision <hassanelmaayati@gmail.com>
2026-09-03 13:25:46 +0300

    .

--- src/components/WeatherSearch/WeatherSearch.jsx
+++ src/components/WeatherSearch/WeatherSearch.jsx
@@ -7,7 +7,7 @@ const WeatherSearch = (props) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // We'll call the fetch function here
+    props.fetchData(city)
     setCity('');
   };
 
