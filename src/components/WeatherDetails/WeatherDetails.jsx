65a946476ee2581f4906c48b25c9eb93acc95e23
LockVision <hassanelmaayati@gmail.com>
2026-09-03 13:26:30 +0300

    .

---
+++ src/components/WeatherDetails/WeatherDetails.jsx
@@ -0,0 +1,17 @@
+// src/components/WeatherDetails/WeatherDetails.jsx
+
+const WeatherDetails = ({weather}) => {
+
+  const {condition, temperature, location} = weather
+
+  return (
+    <section>
+      <h2>Weather Details</h2>
+      <p>Location: {location}</p>
+      <p>Temperature: {temperature}</p>
+      <p>Condition: {condition}</p>
+    </section>
+  );
+};
+
+export default WeatherDetails;
